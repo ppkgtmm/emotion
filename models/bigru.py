e68030c6b854f02932f77af12d0320df932dec5d
ppkgtmm <pkgtm1998@gmail.com>
2021-05-15 23:11:15 +0700

    build stacked bigru

--- models/bigru.py
+++ models/bigru.py
@@ -15,3 +15,18 @@ def build_bigru(hp):
     )
     model.summary()
     return model
+
+def build_stacked_bigru(hp):
+    model = Sequential()
+    model.add(Input(shape=(hp.get('max_len'),)))
+    model.add(Embedding(hp.get('num_words'), hp.get('output_dim')))
+    for i in range(hp.get('n_layers')):
+        model.add(Bidirectional(GRU(hp.get('gru_units'), return_sequences= (i+1 < hp.get('n_layers')))))
+    model.add(Dense(6, activation='softmax'))
+    model.compile(
+        optimizer=get_optimizer(hp.get('optimizer')),
+        loss=get_loss(binary=False),
+        metrics=get_metr(binary=False)
+    )
+    model.summary()
+    return model
