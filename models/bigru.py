2bc9ac5f85bc72206e7c19eb9c133b53b31808e8
ppkgtmm <pkgtm1998@gmail.com>
2021-05-16 19:24:09 +0700

    add stacked bigru with dropout

--- models/bigru.py
+++ models/bigru.py
@@ -30,3 +30,18 @@ def build_stacked_bigru(hp):
     )
     model.summary()
     return model
+
+def build_dstacked_bigru(hp):
+    model = Sequential()
+    model.add(Input(shape=(hp.get('max_len'),)))
+    model.add(Embedding(hp.get('num_words'), hp.get('output_dim')))
+    model.add(Bidirectional(GRU(hp.get('gru_units'), return_sequences= True, dropout=hp.get('drop_rate'))))
+    model.add(Bidirectional(GRU(hp.get('gru_units'))))
+    model.add(Dense(6, activation='softmax'))
+    model.compile(
+        optimizer=get_optimizer(hp.get('optimizer')),
+        loss=get_loss(binary=False),
+        metrics=get_metr(binary=False)
+    )
+    model.summary()
+    return model
